28a67d160e2959eacbcb91d22f4024167945b3aa
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-11-12 14:26:59 -0500

    pages add

--- frontend/src/Pages/Router.jsx
+++ frontend/src/Pages/Router.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { Home, About, Services, Contact, Privacy, Terms } from './'
 
 const Router = () => {
   return (
@@ -7,7 +8,10 @@ const Router = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/terms" element={<Terms />} />            
         </Routes>
     </div>
   )
