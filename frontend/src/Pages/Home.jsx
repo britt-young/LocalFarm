41f519b627fd12deee9f370f438bc2db937efb80
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-11-13 12:11:26 -0500

    footer template and service slider basics

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from '../Components/Header'
 import NavBar from '../Components/NavBar'
+import ServiceSlider from '../Components/ServiceSlider'
+import Footer from '../Components/Footer'
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <Header />
+      <ServiceSlider />
+      <Footer />
     </div>
   )
 }
