7c01ddea7a8330ba9c699ba0da5c1b8dcd27b5b2
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-11-13 11:50:35 -0500

    text change

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -22,10 +22,10 @@ const Header = () => {
               <div className="px-6 text-center text-white md:px-12">
                 <img src={logo} alt="Logo" className="w-80 h-80 mx-auto" />
                 {/* <h1 className="mb-6 text-5xl font-bold"></h1> */}
-                <h3 className="mb-8 text-3xl font-bold">Making farm foods accessible to the community</h3>
+                <h3 className="mb-8 text-3xl font-bold">Growing for you since 1982</h3>
                 <button
                   type="button"
-                  className="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-300 hover:text-neutral-200 focus:border-neutral-300 focus:text-neutral-200 focus:outline-none focus:ring-0 active:border-neutral-300 active:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600"
+                  className="inline-block rounded border-2 bg-sky-700 border-neutral px-6 pb-[6px] pt-2 text-xs font-semibold uppercase tracking-widest leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-300 hover:text-neutral-200 focus:border-neutral-300 focus:text-neutral-200 focus:outline-none focus:ring-0 active:border-neutral-300 active:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600"
                   data-twe-ripple-init=""
                   data-twe-ripple-color="light"
                   onClick={handleClick}
