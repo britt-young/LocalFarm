28435298e8465ccdb2c95d304d7e38d27390940f
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-11-18 14:15:55 -0500

    slider/carousel refactor + css

--- frontend/src/Components/ServiceSlider.jsx
+++ frontend/src/Components/ServiceSlider.jsx
@@ -1,86 +1,67 @@
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { useState } from 'react';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import "../index.css";
+
 import produce from "../assets/imgs/produce.png";
 import meat from "../assets/imgs/meat.png";
 import dairy from "../assets/imgs/dairy.png";
 import grain from "../assets/imgs/grain.png";
 
-import { RxDoubleArrowLeft , RxDoubleArrowRight } from "react-icons/rx";
-
-
-
-const settings = {
-  infinite: true,
-  speed: 500,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  initialSlide: 0,
-  nextArrow: <RxDoubleArrowRight />,
-  prevArrow: <RxDoubleArrowLeft />,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: false,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        initialSlide: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-};
 
 const ServiceSlider = () => {
-  const slides = [
-    { title: "Produce", image: produce },
-    { title: "Meat", image: meat },
-    { title: "Dairy", image: dairy },
-    { title: "Grains", image: grain },
-    { title: "Spices", image: "/path/to/spices.jpg" },
-    { title: "Beverages", image: "/path/to/beverages.jpg" },
-    { title: "Snacks", image: "/path/to/snacks.jpg" },
-    { title: "Frozen", image: "/path/to/frozen.jpg" },
-  ];
+    const images = [
+      { src: produce, alt: 'Image 1', text: 'Produce' },
+      { src: meat, alt: 'Image 2', text: 'Meat' },
+      { src: dairy, alt: 'Image 3', text: 'Dairy' },
+      { src: grain, alt: 'Image 4', text: 'Grains' },
+      { src: 'image5.jpg', alt: 'Image 5', text: 'Spices' },
+      { src: 'image6.jpg', alt: 'Image 6', text: 'Fruits' },
+      { src: 'image7.jpg', alt: 'Image 7', text: 'Vegetables' },
+      { src: 'image8.jpg', alt: 'Image 8', text: 'Image 8 Description' },
+    ];
+  
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    const nextImage = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+  
+    const prevImage = () => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      );
+    };
+  
+    const visibleImages = [
+      images[(currentIndex) % images.length],
+      images[(currentIndex + 1) % images.length],
+      images[(currentIndex + 2) % images.length],
+      images[(currentIndex + 3) % images.length],
+    ];
 
-  return (
-    <div className="max-w-7xl mx-auto px-4 py-8">
-      <p className="text-center text-2xl font-light text-black uppercase">
-        Click a service below to learn more</p>
-      {/* Tailwind for container layout */}
-      <Slider {...settings}>
-        {slides.map((slide, index) => (
-          <div key={index} className="relative group">
-            <img
-              src={slide.image}
-              alt={slide.title}
-              className="w-full h-64 object-cover rounded-sm"
-            />
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-black opacity-40 group-hover:opacity-30 transition-opacity duration-300 rounded-sm" />
-            {/* Text */}
-            <div className="absolute inset-0 flex items-center justify-center">
-              <h3 className="text-2xl font-bold text-white">{slide.title}</h3>
+  return (    
+<div className="carousel-container">
+      <div className="carousel">
+        <div className="carousel-images">
+          {visibleImages.map((image, index) => (
+            <div key={index} className="carousel-item">
+              <img src={image.src} alt={image.alt} className="carousel-image" />
+              <div className="overlay">
+                <div className="overlay-text">{image.text}</div>
+              </div>
             </div>
-          </div>
-        ))}
-      </Slider>
+          ))}
+        </div>
+      </div>
+
+      <div className="controls">
+        <button className="control-btn prev" onClick={prevImage}>
+          <FaArrowLeft />
+        </button>
+        <button className="control-btn next" onClick={nextImage}>
+          <FaArrowRight />
+        </button>
+      </div>
     </div>
   );
 };
--- frontend/src/Components/Testimonials.jsx
+++ frontend/src/Components/Testimonials.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const Testimonials = () => {
   return (
 
-<div class="h-full flex justify-center items-center bg-lime-800">
+<div className="h-full flex justify-center items-center bg-lime-800">
 
 <div className= "m-20">
-<h5 class="mb-4 text-xl font-semibold">James Lesterman</h5>
-    <h6 class="mb-4 font-semibold text-white dark:white">
+<h5 className="mb-4 text-xl font-semibold">James Lesterman</h5>
+    <h6 className="mb-4 font-semibold text-white dark:white">
       Web Developer
     </h6>
-    <p class="mb-4 text-black dark:text-white">
-      <span class="inline-block pe-2 [&>svg]:w-5"
+    <p className="mb-4 text-black dark:text-white">
+      <span className="inline-block pe-2 [&>svg]:w-5"
         ><svg
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -24,17 +24,17 @@ const Testimonials = () => {
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos
       id officiis hic tenetur quae quaerat ad velit ab hic tenetur.
     </p>
-    <ul class="mb-0 flex items-center justify-center">
+    <ul className="mb-0 flex items-center justify-center">
       <li>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          class="h-5 w-5 text-yellow-500">
+          className="h-5 w-5 text-yellow-500">
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-            clip-rule="evenodd" />
+            clipRule="evenodd" />
         </svg>
       </li>
       <li>
@@ -42,11 +42,11 @@ const Testimonials = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          class="h-5 w-5 text-yellow-500">
+          className="h-5 w-5 text-yellow-500">
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-            clip-rule="evenodd" />
+            clipRule="evenodd" />
         </svg>
       </li>
       <li>
@@ -54,11 +54,11 @@ const Testimonials = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          class="h-5 w-5 text-yellow-500">
+          className="h-5 w-5 text-yellow-500">
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-            clip-rule="evenodd" />
+            clipRule="evenodd" />
         </svg>
       </li>
       <li>
@@ -66,26 +66,26 @@ const Testimonials = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          class="h-5 w-5 text-yellow-500">
+          className="h-5 w-5 text-yellow-500">
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-            clip-rule="evenodd" />
+            clipRule="evenodd" />
         </svg>
       </li>
       <li>
         <svg
-          clip-rule="evenodd"
-          fill-rule="evenodd"
+          clipRule="evenodd"
+          fillRule="evenodd"
           fill="currentColor"
-          class="h-5 w-5 text-yellow-500"
-          stroke-linejoin="round"
-          stroke-miterlimit="2"
+          className="h-5 w-5 text-yellow-500"
+          strokeLinejoin="round"
+          strokeMiterlimit="2"
           viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg">
           <path
             d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44zm.678 2.033v11.904l4.707 2.505-.951-5.236 3.851-3.662-5.314-.756z"
-            fill-rule="nonzero" />
+            fillRule="nonzero" />
         </svg>
       </li>
     </ul>
