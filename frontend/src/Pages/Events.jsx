791244f0be354ea6cb93edd4bf1915ace60c31a5
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-11-26 16:16:59 -0500

    title

--- frontend/src/Pages/Events.jsx
+++ frontend/src/Pages/Events.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import EventCalendar from '../Components/EventCalendar'
-import EventsGrid from '../Components/EventsGrid'
+import React from "react";
+import EventCalendar from "../Components/EventCalendar";
+import EventsGrid from "../Components/EventsGrid";
 
 const Events = () => {
   return (
-    <div className='flex'>
-      <EventCalendar />
-      <EventsGrid />
+    <div>
+      <h2 className="flex justify-center mt-20 text-5xl">Upcoming Events</h2>
+      <div className="flex">
+        <EventCalendar />
+        <EventsGrid />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
