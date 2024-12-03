02fc45b5b0e068641830c8feaf0db2ff3316f0ef
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-12-03 12:11:16 -0500

    updated routes using best practices

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,35 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./Pages/Router";
-import NavBar from "./Components/NavBar";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import RootLayout from './layouts/RootLayout'
+
+import Error from './Components/Error'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Services from './Pages/Services'
+import Events from './Pages/Events'
+import Privacy from './Pages/Privacy'
+import Terms from './Pages/Terms'
 
 function App() {
-   return (
-    <>
-      <BrowserRouter>
-      <NavBar />
-        <Router />
-      </BrowserRouter>
-    </>
-  );
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path ='about' element={<About />} />
+      <Route path ='events' element={<Events />} />
+      <Route path ='services' element={<Services />} />
+      {/* Can add nested service routes too */}
+      {/* <Route path ='services/:serviceId' element={<Services />} /> */}
+      <Route path ='privacy' element={<Privacy />} />
+      <Route path ='terms' element={<Terms />} />
+      <Route path ='*' element={<Error />} />
+    </Route>
+    
+  )
+)
+
+  return (
+    <RouterProvider router={router}/>
+  )
 }
 
 export default App;
