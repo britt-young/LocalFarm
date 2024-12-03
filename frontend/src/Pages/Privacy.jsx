4b7438ae6d6c170616a50bab8e3c14c00f22b436
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-12-03 11:52:05 -0500

    privacy policy update

--- frontend/src/Pages/Privacy.jsx
+++ frontend/src/Pages/Privacy.jsx
@@ -3,10 +3,12 @@ import PrivacyStatement from '../Components/PrivacyStatement'
 
 const Privacy = () => {
   return (
-    <div className='mx-40'>
+    <div className='m-40'>
       <PrivacyStatement />
     </div>
   )
 }
 
 export default Privacy
+
+// create a download pdf link for the privacy policy??
