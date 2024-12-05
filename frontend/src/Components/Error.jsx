7dab68b947218ccebf1c22f432f263275734674e
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-12-05 10:42:02 -0500

    404 page styling-centered

--- frontend/src/Components/Error.jsx
+++ frontend/src/Components/Error.jsx
@@ -5,7 +5,7 @@ const Error = () => {
     const navigate = useNavigate()
 
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col h-screen justify-center'>
         <h2 className='mx-auto'> 404 | Page Not Found</h2>
         <br/>
         <button onClick={()=>navigate('/')} className='mx-auto border-2 rounded-lg border-slate-800  p-2'>Go back to homepage</button>
