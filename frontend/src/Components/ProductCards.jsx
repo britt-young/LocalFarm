ca46b00fe24f4eea68d5a4320faabf208c7ff278
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-01-06 11:52:36 -0500

    updated npm & products page

--- frontend/src/Components/ProductCards.jsx
+++ frontend/src/Components/ProductCards.jsx
@@ -1,14 +1,5 @@
 import React from "react";
 
-import veggies from "../assets/imgs/veggies.png";
-import meat from "../assets/imgs/meat.png";
-import dairy from "../assets/imgs/dairy.png";
-import grain from "../assets/imgs/grain.png";
-import spices from "../assets/imgs/spices.png";
-import fruit from "../assets/imgs/fruit.png";
-import eggs from "../assets/imgs/eggs.png";
-import feed from "../assets/imgs/feed.png";
-
 // Initialization for ES Users
 import { Ripple, initTWE } from "tw-elements";
 
@@ -16,150 +7,18 @@ initTWE({ Ripple });
 
 const ProductCards = () => {
   return (
-    <div className="grid lg:grid-cols-8 grid-cols-4 gap-8">
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
-      </div>
-      <div className="text-center">
-        <img
-          src={veggies}
-          className="mx-auto mb-4 w-32 h-20 object-cover rounded-lg"
-          alt="Avatar"
-        />
-        <h5 className="mb-2 text-xl font-medium leading-tight">John Doe</h5>
-        <p className="text-neutral-500 dark:text-neutral-400">Web designer</p>
+    <div>
+      {/* Product Title */}
+      <div className="divide-y-2 divide-solid divide-lime-900 mx-20">
+        <h2 className="flex justify-center m-5 text-5xl">Product Catelog</h2>
+        <div className="px-10">
+          <p className="text-lg m-4 lg:mx-40 px-2 text-center">
+            Explore our selection of fresh products grown with
+            care and harvested at their peak. Each item is carefully cultivated
+            to bring the best flavors and nutrients to your table. Taste the
+            difference of naturally grown, fresh-from-the-farm goodness!
+          </p>
+        </div>
       </div>
     </div>
   );
