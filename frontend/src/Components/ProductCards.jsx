bf8d74c86d00cac5fcc6497821796ca93bebf8e8
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-01-06 14:36:31 -0500

    product cards added-needs refactoring

--- frontend/src/Components/ProductCards.jsx
+++ frontend/src/Components/ProductCards.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import seasons from "../assets/imgs/seasons-icon.svg";
 import veggies from "../assets/imgs/veggies.png";
+
+import winter from "../assets/imgs/winter.svg";
+import spring from "../assets/imgs/spring.svg";
+import summer from "../assets/imgs/summer.svg";
+import fall from "../assets/imgs/fall.svg";
+
 // Initialization for ES Users
 import { Ripple, initTWE } from "tw-elements";
 
@@ -8,56 +14,167 @@ initTWE({ Ripple });
 
 const ProductCards = () => {
   return (
-    <div>
-      {/* Product Title */}
-      <div className="divide-y-2 divide-solid divide-lime-900 mx-20">
-        <h2 className="flex justify-center m-5 text-5xl">Product Catelog</h2>
-        <div className="px-10">
-          <p className="text-lg m-4 lg:mx-40 px-2 text-center">
-            Explore our selection of fresh products grown with care and
-            harvested at their peak. Each item is carefully cultivated to bring
-            the best flavors and nutrients to your table. Taste the difference
-            of naturally grown, fresh-from-the-farm goodness!
-          </p>
-        </div>
-      </div>
-{/* Product Description */}
-      <div class="max-w-sm w-full lg:max-w-full lg:flex m-10">
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img class="w-full" src={veggies} alt="veggies" />
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Grains</div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
-          </div>
-        </div>
-        {/* Season */}
-        <div class="shadow-lg bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-          <div class="mb-8">
-            <div class="flex items-center mb-5">
-            <img class="w-10" src={seasons} alt="seasons" />
-            <p class="text-md text-gray-600 flex items-center ml-2">Seasonal</p>
-            </div>
-            <div class="text-gray-900 font-bold text-xl mb-2">
-              Can coffee make you a better developer?
-            </div>
-            <p class="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div class="flex items-center">
-            <div class="text-sm">
-              <p class="text-gray-900 leading-none">Jonathan Reinink</p>
-              <p class="text-gray-600">Aug 18</p>
+    <div className="grid lg:grid-cols-3 grid-cols-1 gap-2">
+        {/* Product Description */}
+        <div className="max-w-sm w-full lg:max-w-full lg:flex m-10">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full" src={veggies} alt="veggies" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl">Carrots</div>
+              <p className="text-md text-gray-600 mb-4">Vegetable</p>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
             </div>
           </div>
+          {/* Season */}
+          <div className="w-100 shadow-lg bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div className="mb-8">
+              <div className="flex items-center mb-5">
+                <img className="w-10" src={seasons} alt="seasons" />
+                <p className="text-md text-gray-600 flex items-center ml-2">
+                  Seasonal
+                </p>
+              </div>
+              <div className="text-gray-900 font-bold text-xl mb-2">
+                Northern Florida
+              </div>
+              <p className="text-gray-700 text-base">January & February</p>
+              <img className="w-40 m-10" src={winter} alt="seasons" />
+            </div>
+            <div className="flex items-center"></div>
+          </div>
         </div>
-      </div>
+      {/* Product Description */}
+      <div className="max-w-sm w-full lg:max-w-full lg:flex m-10">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full" src={veggies} alt="veggies" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl">Carrots</div>
+              <p className="text-md text-gray-600 mb-4">Vegetable</p>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+          </div>
+          {/* Season */}
+          <div className="w-100 shadow-lg bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div className="mb-8">
+              <div className="flex items-center mb-5">
+                <img className="w-10" src={seasons} alt="seasons" />
+                <p className="text-md text-gray-600 flex items-center ml-2">
+                  Seasonal
+                </p>
+              </div>
+              <div className="text-gray-900 font-bold text-xl mb-2">
+                Northern Florida
+              </div>
+              <p className="text-gray-700 text-base">January & February</p>
+              <img className="w-40 m-10" src={winter} alt="seasons" />
+            </div>
+            <div className="flex items-center"></div>
+          </div>
+        </div>
+        {/* Product Description */}
+        <div className="max-w-sm w-full lg:max-w-full lg:flex m-10">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full" src={veggies} alt="veggies" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl">Carrots</div>
+              <p className="text-md text-gray-600 mb-4">Vegetable</p>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+          </div>
+          {/* Season */}
+          <div className="w-100 shadow-lg bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div className="mb-8">
+              <div className="flex items-center mb-5">
+                <img className="w-10" src={seasons} alt="seasons" />
+                <p className="text-md text-gray-600 flex items-center ml-2">
+                  Seasonal
+                </p>
+              </div>
+              <div className="text-gray-900 font-bold text-xl mb-2">
+                Northern Florida
+              </div>
+              <p className="text-gray-700 text-base">January & February</p>
+              <img className="w-40 m-10" src={winter} alt="seasons" />
+            </div>
+            <div className="flex items-center"></div>
+          </div>
+        </div>
+        {/* Product Description */}
+        <div className="max-w-sm w-full lg:max-w-full lg:flex m-10">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full" src={veggies} alt="veggies" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl">Carrots</div>
+              <p className="text-md text-gray-600 mb-4">Vegetable</p>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+          </div>
+          {/* Season */}
+          <div className="w-100 shadow-lg bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div className="mb-8">
+              <div className="flex items-center mb-5">
+                <img className="w-10" src={seasons} alt="seasons" />
+                <p className="text-md text-gray-600 flex items-center ml-2">
+                  Seasonal
+                </p>
+              </div>
+              <div className="text-gray-900 font-bold text-xl mb-2">
+                Northern Florida
+              </div>
+              <p className="text-gray-700 text-base">January & February</p>
+              <img className="w-40 m-10" src={winter} alt="seasons" />
+            </div>
+            <div className="flex items-center"></div>
+          </div>
+        </div>
+        {/* Product Description */}
+        <div className="max-w-sm w-full lg:max-w-full lg:flex m-10">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full" src={veggies} alt="veggies" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl">Carrots</div>
+              <p className="text-md text-gray-600 mb-4">Vegetable</p>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+          </div>
+          {/* Season */}
+          <div className="w-100 shadow-lg bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div className="mb-8">
+              <div className="flex items-center mb-5">
+                <img className="w-10" src={seasons} alt="seasons" />
+                <p className="text-md text-gray-600 flex items-center ml-2">
+                  Seasonal
+                </p>
+              </div>
+              <div className="text-gray-900 font-bold text-xl mb-2">
+                Northern Florida
+              </div>
+              <p className="text-gray-700 text-base">January & February</p>
+              <img className="w-40 m-10" src={winter} alt="seasons" />
+            </div>
+            <div className="flex items-center"></div>
+          </div>
+        </div>
     </div>
   );
 };
--- frontend/src/Pages/Products.jsx
+++ frontend/src/Pages/Products.jsx
@@ -5,6 +5,18 @@ import ProductCards from '../Components/ProductCards'
 const Products = () => {
   return (
     <div className='m-20'>
+      {/* Product Title */}
+      <div className="divide-y-2 divide-solid divide-lime-900 mx-20">
+        <h2 className="flex justify-center m-5 text-5xl">Product Catelog</h2>
+        <div className="px-10">
+          <p className="text-lg m-4 lg:mx-40 px-2 text-center">
+            Explore our selection of fresh products grown with care and
+            harvested at their peak. Each item is carefully cultivated to bring
+            the best flavors and nutrients to your table. Taste the difference
+            of naturally grown, fresh-from-the-farm goodness!
+          </p>
+        </div>
+      </div>
       <ProductCards/>
     </div>
   )
