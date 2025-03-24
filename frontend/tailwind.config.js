157ab0bdf14b0c430d36c745bcac9e6aea88b8c1
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-03-24 13:07:58 -0400

    tailwind version 4 update

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/tw-elements/js/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("tw-elements/plugin.cjs")],
-  darkMode: "class"
-};
