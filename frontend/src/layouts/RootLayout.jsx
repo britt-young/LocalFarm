972536166093f90c88fda42ff0627f97474f5e1d
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-07-28 12:33:12 -0400

    created scrolling component and added to layout

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import ScrollToTop from '../components/ScrollToTop';
 import NavBar from '../Components/NavBar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Components/Footer'
@@ -6,6 +6,7 @@ import Footer from '../Components/Footer'
 const RootLayout = () => {
   return (
     <div>
+       <ScrollToTop />
         <NavBar/>
         <Outlet />
         <Footer/>
