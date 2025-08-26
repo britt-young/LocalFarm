ec39f6b6d3c6048c06486adca351074b2bcdf567
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-08-26 14:15:42 -0400

    component file temp name change

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -1,17 +1,17 @@
-import ScrollToTop from '../components/ScrollToTop';
-import NavBar from '../Components/NavBar'
-import { Outlet } from 'react-router-dom'
-import Footer from '../Components/Footer'
+import NavBar from "../Temps/NavBar";
+import { Outlet } from "react-router-dom";
+import Footer from "../Temps/Footer";
+import ScrollToTop from "../Temps/ScrollToTop";
 
 const RootLayout = () => {
   return (
     <div>
-       <ScrollToTop />
-        <NavBar/>
-        <Outlet />
-        <Footer/>
+      <ScrollToTop />
+      <NavBar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
